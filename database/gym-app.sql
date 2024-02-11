561af59f62e378f9a19ba8f42892d716e5f8ac73
Noel Michael Talub <jmtalub152@gmail.com>
2024-02-11 17:37:38 +0800

    Changes to be committed: 	modified:   database/gym-app.sql

--- database/gym-app.sql
+++ database/gym-app.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 06, 2024 at 05:53 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Generation Time: Feb 11, 2024 at 08:09 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(45) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `join_date` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `fullname`, `username`, `password`, `join_date`) VALUES
+(1, 'Administrator', 'admin', '$2a$12$O8bVaneBBmfdJa6k1G/boe3ZdvxD0sucBA0SY9iBwd8W9Nx54zs9u', '2/5/2024, 1:30:00 PM');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `announcement`
 --
 
@@ -31,17 +52,16 @@ CREATE TABLE `announcement` (
   `announcement_id` int(11) NOT NULL,
   `client_id` int(45) DEFAULT NULL,
   `title` varchar(60) NOT NULL,
-  `message` varchar(100) NOT NULL,
+  `message` text NOT NULL,
   `date` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `announcement`
 --
 
 INSERT INTO `announcement` (`announcement_id`, `client_id`, `title`, `message`, `date`) VALUES
-(1, 2, 'Payment Notice', 'Please pay you membership as soon as possible', '2/6/2024, 11:15:42 PM'),
-(2, NULL, 'SHOP WILL BE CLOSED', 'Feb 10 Our Shop Will Be Closed', '2/8/2024, 11:15:42 PM');
+(1, NULL, ' Welcome to King\'s Gym!', 'Get ready to achieve your fitness goals with us! We\'re thrilled to have you join our community. Let\'s work together to reach new heights!', '2/4/2024, 3:30:21 PM');
 
 -- --------------------------------------------------------
 
@@ -55,16 +75,9 @@ CREATE TABLE `attendance` (
   `time_in` varchar(15) NOT NULL,
   `time_out` varchar(15) NOT NULL,
   `date` varchar(15) NOT NULL,
-  `status` varchar(2) NOT NULL,
+  `status` tinyint(1) NOT NULL,
   `logs` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `attendance`
---
-
-INSERT INTO `attendance` (`attendance_id`, `client_id`, `time_in`, `time_out`, `date`, `status`, `logs`) VALUES
-(1, 2, '10:45 AM', '3:00 PM', '2/6/2024', '1', '02-02-2024');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -85,14 +98,39 @@ CREATE TABLE `client` (
   `phonenumber` varchar(45) NOT NULL,
   `password` varchar(60) NOT NULL,
   `join_date` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `client`
 --
 
 INSERT INTO `client` (`id`, `fullname`, `address`, `age`, `gender`, `height`, `weight`, `username`, `email`, `phonenumber`, `password`, `join_date`) VALUES
-(2, 'Noel Michael T. Talub', 'Brgy. 16 Laoag City', 22, 'Male', '172', '65', 'noeltalub2', 'noelmichaelttalub@gmail.com', '09166838843', '$2b$15$ROwWA6L1PgSju4K4w04o8eR4tViYvBWmeHeEFH/uFFAjLELzZnpWa', '2/4/2024, 2:27:36 PM');
+(24, 'asdas', NULL, NULL, NULL, NULL, NULL, 'noeltalub2', 'asda@gmail.com', '12312123123', '$2b$15$N4qm28frYTKUV1OPXwjtw.dkA2yQ0I7QvFRpbgxJQDLY3hLvZMTDG', '2/10/2024, 1:59:24 PM');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment`
+--
+
+CREATE TABLE `equipment` (
+  `id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `amount` varchar(60) NOT NULL,
+  `quantity` varchar(60) NOT NULL,
+  `vendor` varchar(60) NOT NULL,
+  `description` varchar(60) NOT NULL,
+  `address` varchar(60) NOT NULL,
+  `phonenumber` varchar(60) NOT NULL,
+  `date` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `equipment`
+--
+
+INSERT INTO `equipment` (`id`, `name`, `amount`, `quantity`, `vendor`, `description`, `address`, `phonenumber`, `date`) VALUES
+(2, '1231312', '50000', '5', 'SS Industries', 'asdasd', '7 Cedarstone Drive', '0912741723', '2019-03-07');
 
 -- --------------------------------------------------------
 
@@ -103,6 +141,7 @@ INSERT INTO `client` (`id`, `fullname`, `address`, `age`, `gender`, `height`, `w
 CREATE TABLE `membership` (
   `membership_id` int(11) NOT NULL,
   `client_id` int(11) NOT NULL,
+  `trainer_id` int(45) NOT NULL,
   `join_date` varchar(45) NOT NULL,
   `membership_service` varchar(45) NOT NULL,
   `membership_plan` varchar(45) NOT NULL,
@@ -111,23 +150,14 @@ CREATE TABLE `membership` (
   `total_amount` varchar(45) NOT NULL,
   `status_change_date` varchar(45) NOT NULL,
   `date_expiration` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `membership`
 --
 
-INSERT INTO `membership` (`membership_id`, `client_id`, `join_date`, `membership_service`, `membership_plan`, `membership_status`, `payment_status`, `total_amount`, `status_change_date`, `date_expiration`) VALUES
-(2, 2, '2/6/2024, 10:38:29 PM', 'Sauna', '3', 'Cancelled', 'Cancelled', '1000', '', NULL),
-(3, 2, '2/6/2024, 10:52:53 PM', 'Cardio', '6', 'Cancelled', 'Cancelled', '1000', '', NULL),
-(4, 2, '2/6/2024, 11:12:58 PM', 'Fitness', '3', 'Cancelled', 'Cancelled', '1000', '', NULL),
-(5, 2, '2/6/2024, 11:14:42 PM', 'Sauna', '12', 'Cancelled', 'Cancelled', '1000', '2/6/2024, 11:15:42 PM', '8/6/2024, 11:15:42 PM'),
-(6, 2, '2/6/2024, 11:31:18 PM', 'Fitness', '1', 'Cancelled', 'Cancelled', '1000', '', NULL),
-(7, 2, '2/6/2024, 11:31:43 PM', 'Fitness', '1', 'Cancelled', 'Cancelled', '1000', '', NULL),
-(8, 2, '2/6/2024, 11:32:08 PM', 'Cardio', '3', 'Expired', 'Paid', '1000', '2/6/2024, 11:15:42 PM', '8/6/2024, 11:15:42 PM'),
-(9, 2, '2/7/2024, 12:23:50 AM', 'Cardio', '6', 'Cancelled', 'Cancelled', '1000', '', NULL),
-(10, 2, '2/7/2024, 12:51:29 AM', 'Fitness', '6', 'Cancelled', 'Cancelled', '1800', '', NULL),
-(11, 2, '2/7/2024, 12:51:52 AM', 'Sauna', '12', 'Waiting for Activation', 'Pending', '5400', '', NULL);
+INSERT INTO `membership` (`membership_id`, `client_id`, `trainer_id`, `join_date`, `membership_service`, `membership_plan`, `membership_status`, `payment_status`, `total_amount`, `status_change_date`, `date_expiration`) VALUES
+(7, 24, 1, '2/10/2024, 1:59:49 PM', 'Powerlifting', '1', 'Activated', 'Paid', '500', '2/10/2024, 1:59:59 PM', '8/10/2024, 1:59:59 PM');
 
 -- --------------------------------------------------------
 
@@ -140,22 +170,50 @@ CREATE TABLE `task` (
   `description` varchar(45) NOT NULL,
   `status` varchar(45) NOT NULL,
   `client_id` int(45) NOT NULL,
+  `trainer_id` int(45) DEFAULT NULL,
   `date` varchar(45) NOT NULL,
-  `time` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `time` varchar(45) NOT NULL,
+  `log_date` varchar(45) DEFAULT NULL,
+  `log_time` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `task`
+-- Table structure for table `trainer`
 --
 
-INSERT INTO `task` (`task_id`, `description`, `status`, `client_id`, `date`, `time`) VALUES
-(5, '10 Push Up', 'In Progress', 2, '2/6/2024', '12:22:57 PM'),
-(6, '1km ThreadMill Walk', 'Done', 2, '2/6/2024', '12:23:56 PM'),
-(7, '11 Pull Up', 'Done', 2, '2/7/2024', '12:52:46 AM');
+CREATE TABLE `trainer` (
+  `trainer_id` int(11) NOT NULL,
+  `fullname` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  `age` int(45) NOT NULL,
+  `gender` varchar(45) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `phonenumber` varchar(45) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `join_date` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `trainer`
+--
+
+INSERT INTO `trainer` (`trainer_id`, `fullname`, `address`, `age`, `gender`, `username`, `email`, `phonenumber`, `password`, `join_date`) VALUES
+(1, 'Emily Johnson', '123 Main Street', 30, 'Female', 'emilyj', 'emilyjohnson@example.com', '1234567890', '$2a$12$7iWhheW2rXV48V4b4.umqOyfkWoRGQxKR/Dn6KHMvzfz2k734Kl8m', '2/2/2024, 3:30:21 PM'),
+(2, 'Michael Smith', '456 Elm Street', 35, 'Male', 'michaels', 'michaelsmith@example.com', '0987654321', '$2a$12$7iWhheW2rXV48V4b4.umqOyfkWoRGQxKR/Dn6KHMvzfz2k734Kl8m', '2/3/2024, 3:30:21 PM'),
+(3, 'Jessica Davis', '789 Oak Street', 28, 'Female', 'jessicad', 'jessicadavis@example.com', '6789012345', '$2a$12$7iWhheW2rXV48V4b4.umqOyfkWoRGQxKR/Dn6KHMvzfz2k734Kl8m', '2/4/2024, 3:30:21 PM');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `announcement`
@@ -180,52 +238,84 @@ ALTER TABLE `client`
   ADD UNIQUE KEY `username` (`username`);
 
 --
+-- Indexes for table `equipment`
+--
+ALTER TABLE `equipment`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `membership`
 --
 ALTER TABLE `membership`
   ADD PRIMARY KEY (`membership_id`),
-  ADD KEY `asd` (`client_id`);
+  ADD KEY `asd` (`client_id`),
+  ADD KEY `trainer` (`trainer_id`);
 
 --
 -- Indexes for table `task`
 --
 ALTER TABLE `task`
   ADD PRIMARY KEY (`task_id`),
-  ADD KEY `client_id` (`client_id`);
+  ADD KEY `client_id` (`client_id`),
+  ADD KEY `trainer_id` (`trainer_id`);
+
+--
+-- Indexes for table `trainer`
+--
+ALTER TABLE `trainer`
+  ADD PRIMARY KEY (`trainer_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `announcement`
 --
 ALTER TABLE `announcement`
-  MODIFY `announcement_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `announcement_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `client`
 --
 ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `equipment`
+--
+ALTER TABLE `equipment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `membership`
 --
 ALTER TABLE `membership`
-  MODIFY `membership_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `membership_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `task`
 --
 ALTER TABLE `task`
-  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `trainer`
+--
+ALTER TABLE `trainer`
+  MODIFY `trainer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -247,13 +337,15 @@ ALTER TABLE `attendance`
 -- Constraints for table `membership`
 --
 ALTER TABLE `membership`
-  ADD CONSTRAINT `asd` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`);
+  ADD CONSTRAINT `asd` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`),
+  ADD CONSTRAINT `membership_ibfk_1` FOREIGN KEY (`trainer_id`) REFERENCES `trainer` (`trainer_id`);
 
 --
 -- Constraints for table `task`
 --
 ALTER TABLE `task`
-  ADD CONSTRAINT `client_id` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`);
+  ADD CONSTRAINT `client_id` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`),
+  ADD CONSTRAINT `task_ibfk_1` FOREIGN KEY (`trainer_id`) REFERENCES `trainer` (`trainer_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
